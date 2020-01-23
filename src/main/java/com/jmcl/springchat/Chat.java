6d7514a690b163a6d8b90b4aae7cdccb8fb66108
johnmcl001 <john.mc-loughlin.3@ucdconnect.ie>
2020-01-23 15:27:31 +0000

    error handling implemented, can now input message and have page update to reflect new chat history

--- src/main/java/com/jmcl/springchat/Chat.java
+++ src/main/java/com/jmcl/springchat/Chat.java
@@ -3,11 +3,18 @@ package com.jmcl.springchat;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 @Data
 @RequiredArgsConstructor
 public class Chat {
 
+    @NotBlank(message="Please specify a user")
     private final String user;
+
+    @Size(min=5, max=120, message="Message must be 5 to 120 characters long")
     private final String message;
 
 }
--- src/main/java/com/jmcl/springchat/web/ChatController.java
+++ src/main/java/com/jmcl/springchat/web/ChatController.java
@@ -1,33 +1,54 @@
 package com.jmcl.springchat.web;
 
 import com.jmcl.springchat.Chat;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Arrays;
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 
+@Data
 @Slf4j
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
+    /*private List<Chat> chatHistory = Arrays.asList(
+            new Chat("john", "Hello"),
+            new Chat("forest", "Hello"),
+            new Chat("john", "how are you?"),
+            new Chat("forest", "I'm good, you?"),
+            new Chat("john", "not too bad")
+    );*/
+
+    private List<Chat> chatHistory = new ArrayList<>();
+
     @GetMapping
     public String showChatHistory(Model model){
-        List<Chat> chatHistory = Arrays.asList(
-                new Chat("john", "Hello"),
-                new Chat("forest", "Hello"),
-                new Chat("john", "how are you?"),
-                new Chat("forest", "I'm good, you?"),
-                new Chat("john", "not too bad")
-        );
 
-        model.addAttribute(chatHistory);
-        log.info(model.toString());
+        model.addAttribute("chatHistory", chatHistory);
+        model.addAttribute("chat", new Chat(null, null));
 
-        return("chat");
+        return ("chat");
     }
+
+    @PostMapping
+    public String processChat(Model model, @Valid @ModelAttribute Chat chat, Errors errors){
+        if (errors.hasErrors()){
+            model.addAttribute("chatHistory", chatHistory);
+            return "chat";
+        }
+        chatHistory.add(chat);
+        model.addAttribute("chatHistory", chatHistory);
+        return "chat";
+    }
+
 }
